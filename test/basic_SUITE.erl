251a899e3a3be0a98b7d21d99c4c4fb4d79fd184
lolmaxlevel <lolmaxlevel@gmail.com>
2024-10-02 22:04:55 +0300

    added tests

--- test/basic_SUITE.erl
+++ test/basic_SUITE.erl
@@ -4,17 +4,24 @@
 -include_lib("stdlib/include/assert.hrl").
 
 -export([all/0]).
--export([regular_data_tests/1]).
+-export([fib_data_tests/1, dist_power_data_tests/1]).
 
 all() ->
-    [regular_data_tests].
+    [fib_data_tests, dist_power_data_tests].
 
 %% Common test requires Config argument in test cases
-regular_data_tests(_) ->
-    test_regular_case(4613732).
+fib_data_tests(_) ->
+    fib_regular_case(4613732).
 
+dist_power_data_tests(_) ->
+    dist_power_regular_case(9183).
 
-test_regular_case(Expected) ->
+dist_power_regular_case(Expected) ->
+    ?assertEqual(Expected, distinct_powers_map:count_distinct_terms()),
+    ?assertEqual(Expected, distinct_powers:count_distinct_terms()).
+
+fib_regular_case(Expected) ->
     ?assertEqual(Expected, fib_even_module:sum_even_fib()),
     ?assertEqual(Expected, fib_even_stream:sum_even_fib()),
-    ?assertEqual(Expected, fib_even_monolith_tail:sum_even_fib()).
+    ?assertEqual(Expected, fib_even_monolith_tail:sum_even_fib()),
+    ?assertEqual(Expected, fib_even_monolith_recursion:sum_even_fib()).
